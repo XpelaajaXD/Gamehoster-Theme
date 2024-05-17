9f8df42e93d4a840de1bdd23d7491394c301df6e
XpelaajaX <139341466+XpelaajaXD@users.noreply.github.com>
2024-05-17 19:37:49 +0300

    Update nests.php

--- resources/lang/en/admin/nests.php
+++ resources/lang/en/admin/nests.php
@@ -2,25 +2,25 @@
 
 return [
     'notices' => [
-        'created' => 'A new nest, :name, has been successfully created.',
-        'deleted' => 'Successfully deleted the requested nest from the Panel.',
-        'updated' => 'Successfully updated the nest configuration options.',
+        'created' => 'Uusi pesä, :name, on luotu onnistuneesti.',
+        'deleted' => 'Pyydetty pesä on poistettu onnistuneesti paneelista.',
+        'updated' => 'Pesän asetukset on päivitetty onnistuneesti.',
     ],
     'eggs' => [
         'notices' => [
-            'imported' => 'Successfully imported this Egg and its associated variables.',
-            'updated_via_import' => 'This Egg has been updated using the file provided.',
-            'deleted' => 'Successfully deleted the requested egg from the Panel.',
-            'updated' => 'Egg configuration has been updated successfully.',
-            'script_updated' => 'Egg install script has been updated and will run whenever servers are installed.',
-            'egg_created' => 'A new egg was laid successfully. You will need to restart any running daemons to apply this new egg.',
+            'imported' => 'Tämä pesä ja siihen liittyvät muuttujat on tuotu onnistuneesti.',
+            'updated_via_import' => 'Tämä pesä on päivitetty annetun tiedoston avulla.',
+            'deleted' => 'Pyydetty pesä on poistettu onnistuneesti paneelista.',
+            'updated' => 'Pesän asetukset on päivitetty onnistuneesti.',
+            'script_updated' => 'Pesän asennusskripti on päivitetty ja se suoritetaan aina, kun palvelimia asennetaan.',
+            'egg_created' => 'Uusi pesä on luotu onnistuneesti. Sinun on käynnistettävä uudelleen kaikki käynnissä olevat daemonit, jotta tämä uusi pesä tulee voimaan.',
         ],
     ],
     'variables' => [
         'notices' => [
-            'variable_deleted' => 'The variable ":variable" has been deleted and will no longer be available to servers once rebuilt.',
-            'variable_updated' => 'The variable ":variable" has been updated. You will need to rebuild any servers using this variable in order to apply changes.',
-            'variable_created' => 'New variable has successfully been created and assigned to this egg.',
+            'variable_deleted' => 'Muuttuja ":variable" on poistettu ja sitä ei enää ole saatavilla palvelimille, kun ne rakennetaan uudelleen.',
+            'variable_updated' => 'Muuttuja ":variable" on päivitetty. Sinun on rakennettava uudelleen kaikki tätä muuttujaa käyttävät palvelimet muutosten käyttämiseksi.',
+            'variable_created' => 'Uusi muuttuja on luotu ja se on onnistuneesti määritelty tälle pesälle.',
         ],
     ],
 ];

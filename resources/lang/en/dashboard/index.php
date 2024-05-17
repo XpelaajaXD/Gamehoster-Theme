fa55202abe5ee09af995e0cb479b08dc280aca4d
XpelaajaX <139341466+XpelaajaXD@users.noreply.github.com>
2024-05-17 19:28:45 +0300

    Update index.php

--- resources/lang/en/dashboard/index.php
+++ resources/lang/en/dashboard/index.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'search' => 'Search for servers...',
-    'no_matches' => 'There were no servers found matching the search criteria provided.',
-    'cpu_title' => 'CPU',
-    'memory_title' => 'Memory',
+    'search' => 'Etsi palvelimia...',
+    'no_matches' => 'Annetuilla hakukriteereillä ei löytynyt yhtään palvelinta.',
+    'cpu_title' =&gt; 'Suoritin',
+    'memory_title' =&gt; 'Muisti',
 ];

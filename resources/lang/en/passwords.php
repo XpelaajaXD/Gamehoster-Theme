e832219b328f2ad1e85430b02457a6d5e4dfbc71
XpelaajaX <139341466+XpelaajaXD@users.noreply.github.com>
2024-05-17 19:22:03 +0300

    Update passwords.php

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -3,17 +3,17 @@
 return [
     /*
     |--------------------------------------------------------------------------
-    | Password Reset Language Lines
+    | Salasanan nollauksen kielirivit
     |--------------------------------------------------------------------------
     |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
+    | Seuraavat kielirivit vastaavat oletusviestejä, joita salasanavälittäjä
+    | antaa salasanan päivitysyrityksen epäonnistuessa, kuten virheellinen
+    | tunnus tai virheellinen uusi salasana.
     |
     */
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'Salasanojen on oltava vähintään kuusi merkkiä pitkiä ja vastattava vahvistusta.',
+    'reset' => 'Salasanasi on nollattu!',
+    'sent' => 'Olemme lähettäneet sinulle sähköpostitse linkin salasanan nollaamiseksi!',
+    'token' => 'Tämä salasanan nollauksen tunnus on virheellinen.',
+    'user' => 'Emme löydä käyttäjää, jolla on kyseinen sähköpostiosoite.',
 ];
